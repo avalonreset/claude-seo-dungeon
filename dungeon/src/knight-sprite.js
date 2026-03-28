082a6715c5c8995b2e782d3c3a956aa7bf53603e
pathtracerunner <108964763+pathtracerunner@users.noreply.github.com>
2026-03-28 17:02:30 -0400

    fix: warrior crop shows full character, buttons 260px wide

--- dungeon/src/knight-sprite.js
+++ dungeon/src/knight-sprite.js
@@ -14,7 +14,7 @@ const CHARACTERS = {
     frameW: 162, frameH: 162,
     idleFrames: 10, runFrames: 8, attackFrames: 7, hitFrames: 3, deathFrames: 7,
     // Crop rect: where the actual character pixels are within the frame
-    cropX: 30, cropY: 50, cropW: 100, cropH: 110
+    cropX: 30, cropY: 25, cropW: 105, cropH: 130
   },
   samurai: {
     name: 'samurai',
